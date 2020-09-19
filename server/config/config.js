c9885c5f7999af3d854d72188d880aaa7db87ce4
Dyangel <dyangeltk17@gmail.com>
2020-09-19 17:02:00 -0500

    upload var mongo_uri

--- server/config/config.js
+++ server/config/config.js
@@ -12,7 +12,7 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev'){
   urlDB = 'mongodb://localhost:27017/cafe';
 }else{
-  urlDB = 'mongodb+srv://dyangel:1234567891234@cafe.pspuk.mongodb.net/cafe?retryWrites=true&w=majority';
+  urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
